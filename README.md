13599ffd6947d163aa98f876e809f40e12a0bb8f
Osaclay <osaosun@gmail.com>
2023-07-17 00:04:31 +0000

    markdown project

---
+++ README.md
@@ -0,0 +1,34 @@
+Web Stack Specialization - Front-end
+
+
+Description💡
+
+Advanced HTML/CSS
+
+Developer Tools
+
+SCSS
+
+Flexbox
+
+Responsive Design
+
+Accesibility
+
+Working with Designers
+
+Design Implementation
+
+Bootstrap
+
+Advanced Javascript
+
+Advanced JQuery
+
+Cookies & Local Storage
+
+ES6
+
+Webpack
+
+Building Portfolio Applications
---
+++ markdown2html.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+A script that converts Markdown to HTML.
+"""
+
+import sys
+import os
+import re
+
+def convert_markdown_to_html(input_file_path, output_file_path):
+    """
+    Converts a Markdown file to HTML and writes the output to a file.
+    """
+    # Check that the Markdown file exists and is a file
+    if not (os.path.exists(input_file_path) and os.path.isfile(input_file_path)):
+        print(f"Missing {input_file_path}", file=sys.stderr)
+        sys.exit(1)
+
+    # Read the Markdown file and convert it to HTML
+    with open(input_file_path, encoding="utf-8") as input_file:
+        html_lines = []
+        for line in input_file:
+            # Check for Markdown headings
+            match = re.match(r"^(#+) (.*)$", line)
+            if match:
+                heading_level = len(match.group(1))
+                heading_text = match.group(2)
+                html_lines.append(f"<h{heading_level}>{heading_text}</h{heading_level}>")
+            else:
+                html_lines.append(line.rstrip())
+
+    # Write the HTML output to a file
+    with open(output_file_path, "w", encoding="utf-8") as output_file:
+        output_file.write("\n".join(html_lines))
+
+if __name__ == "__main__":
+    # Check that the correct number of arguments were provided
+    if len(sys.argv) != 3:
+        print("Usage: ./markdown2html.py <input_file> <output_file>", file=sys.stderr)
+        sys.exit(1)
+
+    # Get the input and output file names from the command-line arguments
+    input_file_path = sys.argv[1]
+    output_file_path = sys.argv[2]
+
+    # Convert the Markdown file to HTML and write the output to a file
+    convert_markdown_to_html(input_file_path, output_file_path)
+
+    # Exit with a successful status code
+    sys.exit(0)
