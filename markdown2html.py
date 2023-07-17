55391afd1a3d9199688ee497db53a36025ae77b5
Osaclay <osaosun@gmail.com>
2023-07-17 01:24:06 +0000

    markdown project

--- markdown2html.py
+++ markdown2html.py
@@ -5,6 +5,18 @@ Script that converts a Markdown file to HTML.
 
 import sys
 import os
+import re
+
+def convert_heading(line):
+    """
+    Converts a Markdown heading line to HTML.
+    """
+    match = re.match(r'^(#+)\s(.+)', line)
+    if match:
+        level = len(match.group(1))
+        heading_text = match.group(2)
+        return f"<h{level}>{heading_text}</h{level}>\n"
+    return line
 
 if __name__ == "__main__":
     if len(sys.argv) < 3:
@@ -17,5 +29,17 @@ if __name__ == "__main__":
     if not os.path.isfile(input_file):
         sys.stderr.write(f"Missing {input_file}\n")
         sys.exit(1)
+
+    # Read the input Markdown file
+    with open(input_file, 'r') as f:
+        markdown_content = f.readlines()
+
+    # Convert Markdown to HTML
+    html_lines = [convert_heading(line) for line in markdown_content]
+    html_content = ''.join(html_lines)
+
+    # Save the HTML output to the output file
+    with open(output_file, 'w') as f:
+        f.write(html_content)
 
     sys.exit(0)
